28cbab95db8364b2229d274a58788b617d9bdcda
LeonardoLG <leonardoleongutierrez27@gmail.com>
2019-10-09 02:27:08 -0500

    Avance 8.10.19

--- src/main/java/com/leonardo/ws/ServicioCore.java
+++ src/main/java/com/leonardo/ws/ServicioCore.java
@@ -8,6 +8,7 @@ package com.leonardo.ws;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Logger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,7 +20,7 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/service/")
 public class ServicioCore {
-
+    private final static Logger LOGGER = Logger.getLogger("com.leonardo.ws.ServicioCore");
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public List<Ubicacion> obtenerUbicaciones() {
@@ -49,7 +50,7 @@ public class ServicioCore {
                 break;
 
         }
-
+        LOGGER.info("Consumo Exitoso");
         return prueba;
     }
 }
